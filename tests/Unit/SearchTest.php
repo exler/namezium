27c46f23831951bda6bcb8539082c09e42d3e15f
Kamil Marut <kamil.marut@pm.me>
2021-02-13 19:05:37 +0100

    test: Add search unit test

---
+++ tests/Unit/SearchTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+use App\Models\Category;
+use App\Models\Generator;
+
+class SearchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_search_driver_works()
+    {
+        $category = Category::factory()->create();
+        $generator = new Generator;
+        $generator->title = "Lorem ipsum";
+        $generator->description = "Dolor sit amet";
+        $generator->category_id = $category->id;
+        $generator->save();
+
+        $result = Generator::search("ipsum")->get();
+        $this->assertNotEmpty($result);
+
+        $result = Generator::search("sit")->get();
+        $this->assertNotEmpty($result);
+
+        $result = Generator::search("not")->get();
+        $this->assertEmpty($result);
+    }
+}
